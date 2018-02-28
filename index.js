7566a6b94f68adddb3e2a39786c693c9feeae91e
Abhicoding <shinganeabhishek@gmail.com>
2018-02-28 15:46:06 +0530

    folder restructuring - added static

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express')
 var path = require('path')
 var app = express()
 
-app.use(express.static('resources'))
+app.use(express.static('resources/static'))
 
 app.get('/', function (req, res) {
   // console.log(res)
-  res.sendFile(path.join(__dirname + '/resources/index.html'))
+  res.sendFile(path.join(__dirname + '/resources/static/index.html'))
 })
 
 app.listen(8000, function () {
