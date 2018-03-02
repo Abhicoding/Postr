5e47ba4c91899857f78ea413937a4b7e84846d24
Abhicoding <shinganeabhishek@gmail.com>
2018-03-02 11:50:21 +0530

    Saving Data to DB

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 var express = require('express')
-var path = require('path')
 var app = express()
+
+var path = require('path')
+
+var randomKey = require('./utilities')
+
 var bodyParser = require('body-parser')
+
+var redis = require('redis')
+var client = redis.createClient()
 
 app.use(express.static('resources/static'))
 app.use(bodyParser.text())
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/resources/static/index.html'))
+})
+
+app.post('/posted', (req, res) => {
+  let key = randomKey.keygen()
+  client.set(key, req.body, redis.print)
+  	res.send()
+})
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!')
+})
+
+client.on('error', function (err) {
+  console.log('Something went wrong ', err)
+})
+
+client.get('post', function (error, result) {
+  if (error) throw error
+  console.log('GET result ->', result)
+})
+
 /*
 app.get('/getTime', function (req, res) {
   //let time = String(Date.now())
@@ -12,16 +43,3 @@ app.get('/getTime', function (req, res) {
   res.send(time)
   // res.sendFile(path.join(__dirname + '/resources/static/index.html'))
 }) */
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/resources/static/index.html'))
-})
-
-app.post('/posted', (req, res) => {
-  console.log(req.body)
-  	res.send()
-})
-
-app.listen(8000, function () {
-  console.log('Example app listening on port 8000!')
-})
